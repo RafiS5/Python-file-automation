92c98cdb18baaa6e4cc119a5aa549129d4ab0e41
RafiS5 <rafi.subhan365@gmail.com>
2024-10-08 09:26:42 +0100

    Updated config file and integrated code to load JSON file as a python dict, which is then looped through for file filtering

--- file_automater.py
+++ file_automater.py
@@ -3,25 +3,37 @@ import time
 import os
 import shutil
 import logging
+import json
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler 
 
 
+
+with open('file_extensions.json' , 'r') as config_file:
+    config = json.load(config_file) # File extension folder succcessfully loaded in ay python dictionary
+
+
 class MyEventHandler(LoggingEventHandler):
     def on_moved(self, event):
-        if event.dest_path.endswith('.stats') or event.dest_path.endswith('.docx.stats') or event.dest_path.endswith('.doc.stats') or event.dest_path.endswith('.stats.docx') or event.dest_path.endswith('.stats.doc'):
-            destination_folder = r"C:\YEAR 2\Probability and Stats"
-            destination = os.path.join(destination_folder,  os.path.basename(event.dest_path))
-            
-            # Delay to ensure OS completes the move action before we try moving it again
-            time.sleep(1)
 
-            # Move the file if it still exists
-            if os.path.exists(event.dest_path):
-                shutil.move(event.dest_path, destination)
-                print(f"File has been moved to {destination}")
-            else:
-                print(f"File {event.dest_path} no longer exists.")
+        for file_ending in config:
+            if event.dest_path.endswith(file_ending):
+                target_file_ending = file_ending
+                target_dest_path = config[file_ending] # Target file path and ending have been found
+                break
+
+        destination_folder = target_dest_path
+        destination = os.path.join(destination_folder,  os.path.basename(event.dest_path))
+            
+        # Delay to ensure OS completes the move action before we try moving it again
+        time.sleep(1)
+
+        # Move the file if it still exists
+        if os.path.exists(event.dest_path):
+            shutil.move(event.dest_path, destination)
+            print(f"File has been moved to {destination}")
+        else:
+            print(f"File {event.dest_path} no longer exists.")
 
     
 
